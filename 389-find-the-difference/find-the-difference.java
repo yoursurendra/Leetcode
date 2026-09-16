c280b90072b90d5dc4974b1a8cd46cd13a25efa9
surendra gurjar <gujjarsurendra388@gmail.com>
2026-09-16 12:18:02 +0530

    Time: 3 ms (42.88%) | Memory: 43 MB (71.56%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] count = new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            count[t.charAt(i)-'a']--;
+            if(count[t.charAt(i)-'a']<0) return t.charAt(i);
+        }
+        return ' ';
+    }
+}
